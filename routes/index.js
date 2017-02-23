1f600e4be2e7b12727de90bfe7f4f27bf37bffb6
C U D D Y <tcuddy@ucsd.edu>
2017-02-23 10:37:53 -0800

    my first commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var projects = require('../projects.json');
 
-/*
- * GET home page.
- */
-
 exports.view = function(req, res){
+    projects['min'] = false;
+  	res.render('index', projects);
+};
+exports.viewMin = function(req, res){
+    projects['min'] = true;
   	res.render('index', projects);
 };
